31d0593bf0a4b7ca48365fd83146c2545620b7be
Lovesnow313 <henry15joy@gmail.com>
2025-06-14 22:12:09 +0600

    Conditional Statements and lists

---
+++ PositiveNegative.py
@@ -0,0 +1,9 @@
+number = int(input("Enter a nuber to check"))
+print("number to be check", number)
+
+if number>0:
+    print("this is a positive number")
+elif number<0:
+    print("this is negative number")
+else:
+    print("this is neutral number")
