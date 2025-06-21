04373a74e46d1af6d812d869743bd29eedca0e93
Lovesnow313 <henry15joy@gmail.com>
2025-06-21 21:41:58 +0600

    Absolute Value

---
+++ abusolute_value.py
@@ -0,0 +1,9 @@
+def absolute_value(num):
+
+    if num >= 0:
+        return num
+    else:
+        return -num
+    
+print("the absolute value of 24:", absolute_value(24))
+print("the absolute value of -54:", absolute_value(-54))
