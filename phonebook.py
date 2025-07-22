8cb2b2cba2711e6d11972693c90f2df69366dea9
Lovesnow313 <henry15joy@gmail.com>
2025-07-22 21:52:42 +0600

    PhoneBook-2

--- phonebook.py
+++ phonebook.py
@@ -43,3 +43,77 @@ def initial_phone_book():
                      temp[j] = "none"
 
         phoneBook.append[temp]        
+    
+    print(phoneBook)
+    return phoneBook
+
+def menu():
+   
+   print("...................................................................")
+   print("\t\t\tSMARTPHONE DIRECTORY", flush=False")
+
+   print("...............................................................")
+
+    print("\tYou can now perform the following operations on this phonebook\n")
+
+    print("1. Add a new contact")
+
+    print("2. Remove an existing contact")
+
+    print("3. Delete all contacts")
+
+    print("4. Search for a contact")
+
+    print("5. Display all contacts")
+
+    print("6. Exit phonebook")
+
+    choice = int(input("Enter your choice"))
+
+    return choice
+
+def addContact(pb):
+
+    dip = []
+
+    for i in range(len(pb[0]))
+        
+        if i == 0:
+            dip.appened(str(input("Enter the name")))
+         if i == 1:
+            dip.appened(str(input("Enter the number")))
+         if i == 2:
+            dip.appened(str(input("Enter the email address")))
+         if i == 3:
+            dip.appened(str(input("Date of birth dd/mm/yy")))
+         if i == 4:
+            dip.appened(str(input("Enter the category Family/ Friend/ Work/ Other")))
+
+    pb.appened(dip)
+
+    return pb
+
+def remove_existing(pb):
+
+    query = str(input("Please enter the name of the contact you wish to remove."))
+
+    temp = 0
+
+    for i in range(len(pb)):
+    
+        if query == pb[i][0]:
+           temp = temp+1
+
+           print(pb.pop(i))
+           print("This contact has been removed")
+
+           return pb
+        if temp == 0:
+            
+            print("Sorry ypu have entered an invaild query")
+
+            return pb
+
+def remove_all(pb):
+    
+    return pb.clear()
